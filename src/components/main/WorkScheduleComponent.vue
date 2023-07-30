5ee73168c9e321faa8e4ec6845e736b59a4d2ac0
dionis <denis.pascaru95@gmail.com>
2023-07-30 18:42:35 +0300

    Refactoring components design

---
+++ src/components/main/WorkScheduleComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="work-schedule">
+    <div class="container">
+      <div class="section border-top border-bottom">
+        <h2>График работы</h2>
+        <div class="work-schedule-activity">
+          <h3>Понедельник / Пятница</h3>
+          <span>08:00 - 18:00</span>
+        </div>
+        <div class="work-schedule-activity">
+          <h3>Суббота</h3>
+          <span>08:00 - 14:00</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkScheduleComponent',
+}
+</script>
