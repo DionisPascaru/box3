48eab1203557cfd38d7c3320beeb8c4ae2c51a36
dionis <denis.pascaru95@gmail.com>
2023-08-06 16:22:49 +0300

    Add components animation

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -1,29 +1,5 @@
 <template>
   <footer>
-    <div class="footer">
-      <div class="container">
-        <div class="subscribe-block">
-          <div class="subscribe">
-            <h3>подписывайся на наши соц сеть</h3>
-            <div class="subscribe-media-items">
-              <div class="subscribe-media-item">
-                <img :src="require('@/assets/images/facebook.svg')">
-              </div>
-              <div class="subscribe-media-item">
-                <img :src="require('@/assets/images/instagram.svg')">
-              </div>
-              <div class="subscribe-media-item">
-                <img :src="require('@/assets/images/tik-tok.svg')">
-              </div>
-              <div class="subscribe-media-item">
-                <img :src="require('@/assets/images/telegram.svg')">
-              </div>
-            </div>
-          </div>
-          <div class="subscribe-red-line"></div>
-        </div>
-      </div>
-    </div>
     <div class="copyright">
       <div class="container">
         <a href="https://reabovstudio.com/" target="_blank">by reabovstudio</a>
